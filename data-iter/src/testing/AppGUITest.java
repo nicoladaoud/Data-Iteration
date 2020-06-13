50b4edc572c023547c3b3fbcc0faa666c13e83a3
Nicola Daoud <40409521+nicoladaoud@users.noreply.github.com>
2020-06-13 00:39:16 -0700

    Update AppGUITest.java

--- data-iter/src/testing/AppGUITest.java
+++ data-iter/src/testing/AppGUITest.java
@@ -2,23 +2,46 @@ package testing;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import org.junit.jupiter.api.Test;
+
+import AppGUI;
+
+import static org.junit.jupiter.api.Assertions.*;
+
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 class AppGUITest {
 
-	@BeforeAll
-	static void setUpBeforeClass() throws Exception {
-	}
-
-	@BeforeEach
-	void setUp() throws Exception {
-	}
-
+	AppGUI gui = new AppGUI();
+	
 	@Test
-	void test() {
-		fail("Not yet implemented");
+	/**
+	 * Check if user info is empty.
+	 */
+	void test_initial_userInfo() {
+		assertEquals("", gui.getUserEmail());
+		assertEquals("", gui.getUserFirstName());
+	}
+	
+	
+	@Test
+	/**
+	 * Check if the PDF file viewer is not corrupted.
+	 */
+	void test_initial_pdf_viwer() {
+		assertEquals(null, gui.getController());
+		assertEquals(null, gui.getFilePath());
+	}
+	
+	@Test
+	/**
+	 * Check if the admin information is corrupted.
+	 */
+	void test_admin_status() {
+		assertEquals("", gui.getAdminId());
+		assertEquals("", gui.getAdminPass());
 	}
 
 }
